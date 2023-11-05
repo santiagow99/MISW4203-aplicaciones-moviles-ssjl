0163ebf687ce9eebd55f59c12e1ca6cd114013b6
Jhon Andres Parra Rodriguez <ja.parrar12@uniandes.edu.co>
2023-11-05 17:38:18 -0500

    screen album info show info

--- app/src/main/java/com/example/vinyls_jetpack_application/ui/AlbumDetailActivity.kt
+++ app/src/main/java/com/example/vinyls_jetpack_application/ui/AlbumDetailActivity.kt
@@ -8,22 +8,39 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.vinyls_jetpack_application.databinding.AlbumDetailItemBinding
 import com.example.vinyls_jetpack_application.R
+import com.example.vinyls_jetpack_application.databinding.ActivityAlbumDetailBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AlbumDetailActivity : AppCompatActivity() {
 
-    private var albumId: Int? = null;
+    private var albumId: Int = 0;
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("onCreate Activity", "0")
         super.onCreate(savedInstanceState)
-        val binding = AlbumDetailItemBinding.inflate(layoutInflater)
+        val binding = ActivityAlbumDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        albumId = intent.extras?.getInt("albumId")
-
+        albumId = intent.extras?.getInt("albumId") ?: 0
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_album_detail_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        Log.d("act", navController.toString())
 
     }
 
-    fun getAlbumId(): Int? {
+    fun getAlbumId(): Int {
         return this.albumId
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
