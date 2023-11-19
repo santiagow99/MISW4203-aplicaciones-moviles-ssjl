e861f505271478bc12dab7618662598dbc66e61d
Jhon Andres Parra Rodriguez <ja.parrar12@uniandes.edu.co>
2023-11-19 09:13:43 -0500

    Create page add album to artist

---
+++ app/src/main/java/com/example/vinyls_jetpack_application/ui/adapters/AddAlbumArtistAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.vinyls_jetpack_application.ui.adapters
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class AddAlbumArtistAdapter {
+
+
+}
